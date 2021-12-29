2359caed972d0a859d8aa7f1a17f7b3959b7b4d6
Xabi <xaabi6@users.noreply.github.com>
2021-12-29 11:28:17 +0100

    Actualizar año del copyright
    Se actualiza el año del copyright.

--- pluginCode/src/com/ejie/uda/exporters/JacksonExporter.java
+++ pluginCode/src/com/ejie/uda/exporters/JacksonExporter.java
@@ -1,5 +1,5 @@
 /*
-* Copyright 2021 E.J.I.E., S.A.
+* Copyright 2022 E.J.I.E., S.A.
 *
 * Licencia con arreglo a la EUPL, Versión 1.1 exclusivamente (la «Licencia»);
 * Solo podrá usarse esta obra si se respeta la Licencia.
