3718e9bf89c4449c7d9380f940ba442dac4dbc5a
marcos9818 <131156200+marcos9818@users.noreply.github.com>
2024-08-08 11:13:36 +0200

    Paso 5 completo

--- VentanaPaso5.py
+++ VentanaPaso5.py
@@ -3,7 +3,12 @@ import tkinter as tk
 from customtkinter import *
 import plugin.utils as utl
 from pathlib import Path
+from copier import Worker
+import logging
+from datetime import datetime
 self = CTk()
+
+ruta_classes = utl.readConfig("RUTA", "ruta_classes")
 
 class Paso5(CTk):
     def __init__(self):
@@ -12,6 +17,7 @@ class Paso5(CTk):
         self.title("Crear nueva aplicación")
         self.geometry("900x700")
         self.resizable(width=False, height=False)
+        self.rowconfigure(5, weight=1)
         
         # Configurar el color de fondo de la ventana
         self.config(bg="#FFFFFF")
@@ -45,69 +51,45 @@ class Paso5(CTk):
 
 
         # EAR to bind
-        ear_label = CTkLabel(self, text="EAR a vincular:", bg_color='#E0E0E0', text_color="black", font=("Arial", 12, "bold"))
-        ear_label.grid(row=2, column=0, sticky="w", padx= (20,20), pady=5)
-        self.ear_entry = CTkEntry(self, bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', height=2.5, border_width=3, text_color="black" )
+        ear_label = CTkLabel(self, text="EAR a vincular:", bg_color='#FFFFFF', text_color="black", font=("Arial", 12, "bold"))
+        ear_label.grid(row=2, column=0, sticky="w", padx= (20,20), pady=(30, 0))
+        self.ear_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black" )
         self.ear_entry.grid(row=2, column=1, padx=(30,180), pady=(5, 2), sticky="ew")
         self.ear_entry.insert(0, textRutaNegocio)
-        ear_button = CTkButton(self, text="Buscar Proyecto", command= lambda : self.buscar_archivos(self.selectDirectory(self.ear_entry.get())), bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
+        ear_button = CTkButton(self, text="Buscar Proyecto", command= lambda : self.buscar_archivos(self.selectDirectory(self.ear_entry.get())), bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', text_color="black", font=("Arial", 12, "bold"), width= 100, height=23)
         ear_button.grid(row=2, column=1, sticky="e", padx=(100, 20))
 
         # WAR name
-        war_name_label = CTkLabel(self, text="Nombre del WAR:", bg_color='#E0E0E0', text_color="black", font=("Arial", 12, "bold"))
-        war_name_label.grid(row=3, column=0, sticky="w", padx= (20,20), pady=5)
-        war_name_entry = CTkEntry(self, bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', height=2.5, border_width=3, text_color="black" )
-        war_name_entry.grid(row=3, column=1, padx=(30,180), pady=(5, 2), sticky="ew")
+        ejb_name_label = CTkLabel(self, text="Nombre del proyecto EJB:", bg_color='#FFFFFF', text_color="black", font=("Arial", 12, "bold"))
+        ejb_name_label.grid(row=3, column=0, sticky="w", padx= (20,20), pady=5)
+        self.ejb_name_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black" )
+        self.ejb_name_entry.grid(row=3, column=1, padx=(30,180), pady=(5, 2), sticky="ew")
 
         # Full WAR name
-        full_war_name_label = CTkLabel(self, text="Nombre Completo del WAR:", bg_color='#E0E0E0', text_color="black", font=("Arial", 12, "bold"))
-        full_war_name_label.grid(row=4, column=0, sticky="w", padx= (20,20), pady=5)
-        full_war_name_entry = CTkEntry(self, bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', height=2.5, border_width=3, text_color="black" )
-        full_war_name_entry.grid(row=4, column=1, padx=(30,180), pady=(5, 2), sticky="ew")
+        full_ejb_name_label = CTkLabel(self, text="Nombre Completo del modulo EJB:", bg_color='#FFFFFF', text_color="black", font=("Arial", 12, "bold"))
+        full_ejb_name_label.grid(row=4, column=0, sticky="w", padx= (20,20), pady=5)
+        full_ejb_name_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black" )
+        full_ejb_name_entry.grid(row=4, column=1, padx=(30,180), pady=(5, 2), sticky="ew")
 
         # Buttons
-        buttons_frame = ctk.CTkFrame(self, fg_color="#E0E0E0", bg_color="#E0E0E0")
+        buttons_frame = ctk.CTkFrame(self, fg_color="#FFFFFF", bg_color="#FFFFFF")
         buttons_frame.grid(row=5, column=0, columnspan=2, pady=10)
         
-        back_button = CTkButton(buttons_frame, text="Back", bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
-        back_button.grid(row=0, column=0, padx=5)
-        next_button = CTkButton(buttons_frame, text="Next", state="disabled", bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
-        next_button.grid(row=0, column=1, padx=5)
-        finish_button = CTkButton(buttons_frame, text="Finish", bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
-        finish_button.grid(row=0, column=2, padx=5)
-        cancel_button = CTkButton(buttons_frame, text="Cancel", bg_color='#E0E0E0', fg_color='#69a3d6', border_color='#69a3d6', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
-        cancel_button.grid(row=0, column=3, padx=5)
-
-
-    def update_default_language_options(self):
-            idiomas_seleccionados = []
-
-                # Iterar sobre los checkbox y agregar los idiomas seleccionados a la lista
-            for lang_option, lang_var in zip(self.language_options, self.language_vars):
-                idiomas_seleccionados.append(lang_option)
-
-            self.default_language_combobox = CTkComboBox(self.idiomas_inner_frame, values= idiomas_seleccionados, command=self.validarIdioma)
-            self.default_language_combobox.grid(row=7, column=1, padx=(0, 20), pady=(25, 2), sticky="ew")
-            self.default_language_combobox.set(idiomas_seleccionados[0] if idiomas_seleccionados else "")
-            self.default_language_combobox._values = idiomas_seleccionados
-
-            return self.default_language_combobox   
-    
-    def validarIdioma(self, idiomaSeleccionado):
-
-        for lang_option, lang_var in zip(self.language_options, self.language_vars):
-            if lang_var.get() == FALSE:
-                if lang_option == idiomaSeleccionado:
-                    self.configuration_warning.configure(text="El idioma por defecto('"+idiomaSeleccionado+"'), no esta seleccionado")
-                    return FALSE
-        self.configuration_warning.configure(text="")
+        back_button = CTkButton(buttons_frame, text="Atrás", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
+        back_button.grid(row=0, column=0, padx=(400, 5), pady = (400, 0))
+        next_button = CTkButton(buttons_frame, text="Siguiente", state="disabled", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
+        next_button.grid(row=0, column=1, padx=5, pady = (400, 0))
+        finish_button = CTkButton(buttons_frame, text="Terminar", command= lambda: self.save_to_yaml(), bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
+        finish_button.grid(row=0, column=2, padx=5, pady = (400, 0))
+        cancel_button = CTkButton(buttons_frame, text="Cancelar", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', text_color="black", font=("Arial", 12, "bold"), width= 100, height=25)
+        cancel_button.grid(row=0, column=3, padx=5, pady = (400, 0))
 
     def buscar_archivos(self, ruta_personalizada = None):
         files = None
         """Busca archivos con terminación 'Classes' en la misma ruta del script Python."""
         if ruta_personalizada == None:
             try:
-                files = [file for file in os.listdir(ruta_classes) if file.endswith("Classes")]
+                files = [file for file in os.listdir(ruta_clas5ses) if file.endswith("Classes")]
             except:
                 print("No encontro la ruta: " + ruta_classes)
             self.mostrar_resultados(files, ruta_classes)
@@ -125,44 +107,108 @@ class Paso5(CTk):
             par = Path(directory)
             return str(par.parent)
         
-    def mostrar_resultados(self, files,ruta):
-        """Muestra los archivos encontrados en una nueva ventana con radiobuttons."""
-
+    def mostrar_resultados_scrollbar(self, files, ruta):
         resultados_window = ctk.CTkToplevel(self)
         resultados_window.title("Resultados de Búsqueda")
         resultados_window.geometry("600x300")
+        resultados_window.configure(corner_radius=10, fg_color="#FFFFFF", border_color="#84bfc4", border_width=4)
         resultados_window.attributes('-topmost', True)  # Asegura que la ventana emergente se muestre al frente
 
         # Variable para almacenar el archivo seleccionado
         selected_file = tk.StringVar(value=None)
 
-        # Frame para contener los radiobuttons
-        file_frame = ctk.CTkFrame(resultados_window)
-        file_frame.pack(fill="both", expand=True)
+        # Frame para contener los radiobuttons con scrollbar
+        scrollbar_container = ctk.CTkFrame(resultados_window, fg_color="#FFFFFF", bg_color="#FFFFFF")
+        scrollbar_container.grid(row=1, column=0, columnspan=3, sticky="nsew")
+        scrollbar_container.grid_columnconfigure(0, weight=1)
+        scrollbar_container.grid_rowconfigure(0, weight=1)
+        
+        scrollbar_resumen = ctk.CTkScrollableFrame(scrollbar_container, fg_color="#E0E0E0", scrollbar_fg_color="#E0E0E0")
+        scrollbar_resumen.pack(fill="both", expand=True, padx=10, pady=10)
 
-        desc_label = CTkLabel(file_frame, text="Seleccione un Archivo ")
-        desc_label.grid(row=0, column=0, columnspan=3, pady=(5, 1), padx=20, sticky="w")
-        if (ruta != ''):
-            desc_label2 = CTkLabel(file_frame, text="(" + ruta +")")
-            desc_label2.grid(row=1, column=0, columnspan=3, pady=(0,2), padx=30, sticky="w")
+        # Descripción
+        desc_label = ctk.CTkLabel(scrollbar_resumen, text="Seleccione un EAR", text_color="black")
+        desc_label.pack(fill="x", pady=(5, 1), padx=20, anchor="w")
+        
+        if ruta != '':
+            desc_label2 = ctk.CTkLabel(scrollbar_resumen, text="(" + ruta + ")", text_color="black")
+            desc_label2.pack(fill="x", pady=(0, 2), padx=30, anchor="w")
 
         # Añadir radiobuttons para cada archivo
-        if(files != None and len(files) > 0):
-            for index, file in enumerate(files):
-                radiobutton = ctk.CTkRadioButton(file_frame, text=file, variable=selected_file, value=file)
-                radiobutton.grid(row=index + 3, column=0, sticky="w", padx=60, pady=3)
+        if files and len(files) > 0:
+            for file in files:
+                radiobutton = ctk.CTkRadioButton(scrollbar_resumen, text=file, variable=selected_file, value=file, border_color='#84bfc4', fg_color='#84bfc4', text_color="black", font=("Arial", 12, "bold"))
+                radiobutton.pack(fill="x", padx=60, pady=3, anchor="w")
+        else:
+            texto = "Esta ruta no contiene ningún EarClasses"  
+            desc_label3 = CTkLabel(scrollbar_container, text=texto,text_color="red")
+            desc_label3.pack(fill="x", pady=(0, 2), padx=30, anchor="w")
 
         # Botones de acción en el pie de página
-        button_frame = ctk.CTkFrame(resultados_window)
-        button_frame.pack(fill="x", pady=20)
-            
-        buscar_button = ctk.CTkButton(button_frame, text="Buscar", command= lambda: self.open_file_explorer(resultados_window)) 
+        button_frame = ctk.CTkFrame(resultados_window, fg_color="#FFFFFF", border_color="#84bfc4")
+        button_frame.grid(row=2, column=0, columnspan=3, sticky="ew", pady=20)
+        
+        buscar_button = ctk.CTkButton(button_frame, text="Buscar", command=lambda: self.open_file_explorer(resultados_window), fg_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
         buscar_button.pack(side="left", padx=10, expand=True)
-            
-        cancel_button = ctk.CTkButton(button_frame, text="Cancelar", command=resultados_window.destroy)
+        
+        cancel_button = ctk.CTkButton(button_frame, text="Cancelar", command=resultados_window.destroy, fg_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
         cancel_button.pack(side="right", padx=10, expand=True)
-        accept_button = ctk.CTkButton(button_frame, text="Aceptar", command=lambda: self.aceptar(resultados_window, selected_file.get(), ruta))
+        
+        accept_button = ctk.CTkButton(button_frame, text="Aceptar", command=lambda: self.aceptar(resultados_window, selected_file.get(), ruta), fg_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
         accept_button.pack(side="right", padx=10, expand=True)
+
+    def mostrar_resultados(self, files,ruta):
+
+
+
+        if files != None and len(files) > 6:
+            self.mostrar_resultados_scrollbar(files, ruta)
+
+        else:
+
+            """Muestra los archivos encontrados en una nueva ventana con radiobuttons."""
+
+            resultados_window = ctk.CTkToplevel(self)
+            resultados_window.title("Resultados de Búsqueda")
+            resultados_window.geometry("600x300")
+            resultados_window.configure(corner_radius=10, fg_color="#FFFFFF", border_color="#84bfc4", border_width=4)
+            
+            resultados_window.attributes('-topmost', True)  # Asegura que la ventana emergente se muestre al frente
+
+            # Variable para almacenar el archivo seleccionado
+            selected_file = tk.StringVar(value=None)
+
+            # Frame para contener los radiobuttons
+            file_frame = ctk.CTkFrame(resultados_window, fg_color="#FFFFFF", border_color="#84bfc4")
+            file_frame.pack(fill="both", expand=True)
+            desc_label = CTkLabel(file_frame, text="Seleccione un EAR ", text_color= "black")
+            desc_label.grid(row=0, column=0, columnspan=3, pady=(5, 1), padx=20, sticky="w")
+            if (ruta != ''):
+                desc_label2 = CTkLabel(file_frame, text="(" + ruta +")" , text_color= "black")
+                desc_label2.grid(row=1, column=0, columnspan=3, pady=(0,2), padx=30, sticky="w")
+
+            # Añadir radiobuttons para cada archivo
+            if(files != None and len(files) > 0):
+                for index, file in enumerate(files):
+                    radiobutton = ctk.CTkRadioButton(file_frame, text=file, variable=selected_file, value=file, border_color='#84bfc4', fg_color='#84bfc4', text_color= "black", font=("Arial", 12, "bold"))
+                    radiobutton.grid(row=index + 3, column=0, sticky="w", padx=60, pady=3)
+            else:    
+    
+                texto = "Esta ruta no contiene ningún EarClasses"  
+                desc_label3 = CTkLabel(file_frame, text=texto,text_color="red")
+                desc_label3.grid(row=3, column=0, columnspan=3, pady=(0,2), padx=30, sticky="w")
+            # Botones de acción en el pie de página
+            button_frame = ctk.CTkFrame(resultados_window, fg_color="#FFFFFF", border_color="#84bfc4")
+            button_frame.pack(fill="x", pady=20)
+                
+            buscar_button = ctk.CTkButton(button_frame, text="Buscar", command= lambda: self.open_file_explorer(resultados_window), fg_color='#84bfc4',  hover_color='#41848a', text_color= "black", font=("Arial", 12, "bold")) 
+            buscar_button.pack(side="left", padx=10, expand=True)
+                
+            cancel_button = ctk.CTkButton(button_frame, text="Cancelar", command=resultados_window.destroy, fg_color='#84bfc4',  hover_color='#41848a', text_color= "black", font=("Arial", 12, "bold"))
+            cancel_button.pack(side="right", padx=10, expand=True)
+            accept_button = ctk.CTkButton(button_frame, text="Aceptar", command=lambda: self.aceptar(resultados_window, selected_file.get(),ruta), fg_color='#84bfc4',  hover_color='#41848a', text_color= "black", font=("Arial", 12, "bold"))
+            accept_button.pack(side="right", padx=10, expand=True) 
+
 
     def open_file_explorer(self, frame):
         # Esta función se llama cuando el usuario hace clic en "Buscar"
@@ -190,10 +236,49 @@ class Paso5(CTk):
             print("No se seleccionó ningún archivo.")
 
 
+    def save_to_yaml(self):      
+
+        inicio = datetime.now()
+        array_proyect = self.ear_entry.get().split("/")
+        proyect_name = array_proyect[len(array_proyect)-1].split("EARClasses")
+        yaml_data = {
+            "project_name": proyect_name[0],
+            "ejb_project_name": self.ejb_name_entry.get(),
+        }
+
+        rutaPath = utl.rutaActual(__file__)
+        directorio_actual = rutaPath + "\\templates\\proyectoPaso5"
+        filesExcludes = []
+
+        #destinoPath = self.entry_location.get()
+        #if(destinoPath == ''):
+        destinoPath = rutaPath
+        now = datetime.now()
+        dates = now.strftime('%d-%b-%Y %H:%M:%S') 
+        print('Inicio: proyecto Creando... ' +yaml_data["project_name"]+ yaml_data["ejb_project_name"]+  "EJB")    
+        with Worker(src_path=directorio_actual,overwrite=True, dst_path=destinoPath, data=yaml_data,exclude=filesExcludes) as worker:
+            logging.info('Inicio: Crear proyecto: ' + yaml_data["ejb_project_name"])
+            worker.template.version = ": 1.0 Paso 1 ::: "+dates
+            worker.run_copy()
+            logging.info('Fin: Crear proyecto: ' + yaml_data["project_name"]+ yaml_data["ejb_project_name"]+  "EJB")
+            #guardar ultima ruta creada5
+            utl.writeConfig(
+                "RUTA", {"ruta_classes":destinoPath,"ruta_war":destinoPath,"ruta_ultimo_proyecto":destinoPath})
+        #self.ocultarSpinner()
+        #self.close_loading_frame()
+        print('Fin: proyecto Creado: ' + yaml_data["project_name"]+ yaml_data["ejb_project_name"]+  "EJB")
+        fin = datetime.now()
+        logging.info('Tiempo: proyecto Creado en: ' + str((fin-inicio).total_seconds()) + " segundos")
+        now = datetime.now()
+        dates = now.strftime('%d-%b-%Y %H:%M:%S')
+        print(F"Final: paso 1 creado ::: "+dates,file=sys.stderr)
+        sys.stderr.flush()
+    
+        #self.ventana_final_popup()
+
+
     
 if __name__ == "__main__":
 
     app = Paso5()
     app.mainloop()
-
-
