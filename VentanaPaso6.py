140c580ecc22097cbfca81d780dbc0df71b8f00a
marcos9818 <131156200+marcos9818@users.noreply.github.com>
2024-08-14 08:13:46 +0200

    Paso 6 by 7 no terminados

--- VentanaPaso6.py
+++ VentanaPaso6.py
@@ -1,111 +1,119 @@
 import customtkinter as ctk
 import tkinter as tk
+from customtkinter import *
+import plugin.utils as utl
+from pathlib import Path
 
+class VentanaPaso6(CTk):
+    def __init__(self):
+        super().__init__()
+        # Configurar la ventana principal
+        self.title("Crear nueva aplicación")
+        self.geometry("900x700")
+        self.resizable(width=False, height=False)
+        self.config(bg="#FFFFFF")
 
+        self.columnconfigure(1, weight=1)
 
-# Create the main window
-root2 = ctk.CTk()
-root2.title("Generar EJB Cliente")
-root2.geometry("500x600")
+        configuration_frame = CTkFrame(self, bg_color="#FFFFFF")
+        configuration_frame.grid(row=0, column=0, columnspan=3, sticky="ew")
 
-def create_widgets_2():
-    # Title label
-    title_label = ctk.CTkLabel(root2, text="Generar EJB Cliente", font=ctk.CTkFont(size=16, weight="bold"))
-    title_label.grid(row=0, column=0, columnspan=2, pady=10)
+        configuration_label = CTkLabel(configuration_frame, text="Generar EJB Cliente", font=("Arial", 14, "bold"))
+        configuration_label.grid(row=0, column=0, columnspan=3, pady=(10, 5), padx=10, sticky="w")
 
-    # Subtitle label
-    subtitle_label = ctk.CTkLabel(root2, text="Este Wizard el EJB Cliente de un servicio existente")
-    subtitle_label.grid(row=1, column=0, columnspan=2)
+        description_label = CTkLabel(configuration_frame, text="Este Wizard el EJB Cliente de un servicio existente")
+        description_label.grid(row=1, column=0, columnspan=3, pady=(5, 5), padx=10, sticky="w")
 
-    # EJB project container
-    ejb_container_label = ctk.CTkLabel(root2, text="Proyecto EJB contenedor:")
-    ejb_container_label.grid(row=2, column=0, sticky="w", pady=5)
-    ejb_container_entry = ctk.CTkEntry(root2, width=250)
-    ejb_container_entry.grid(row=2, column=1, pady=5, padx=10, sticky="w")
-    ejb_container_button = ctk.CTkButton(root2, text="Buscar Proyecto")
-    ejb_container_button.grid(row=2, column=1, sticky="e", padx=10)
+        ejb_container_label = CTkLabel(self, text="Proyecto EJB contenedor:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        ejb_container_label.grid(row=2, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
+        ejb_container_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        ejb_container_entry.grid(row=2, column=1, padx=(10, 5), pady=(10, 2), sticky="ew")
+        ejb_container_button = CTkButton(self, text="Buscar Proyecto", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        ejb_container_button.grid(row=2, column=2, sticky="e", padx=(5, 10))
 
-    # EJB Remote Type
-    ejb_remote_type_label = ctk.CTkLabel(root2, text="Tipo de EJB Remoto:")
-    ejb_remote_type_label.grid(row=3, column=0, sticky="w", pady=5)
-    
-    ejb_remote_type_frame = ctk.CTkFrame(root2)
-    ejb_remote_type_frame.grid(row=3, column=1, pady=5, padx=10, sticky="w")
+        ejb_remote_type_label = CTkLabel(self, text="Tipo de EJB Remoto:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        ejb_remote_type_label.grid(row=3, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
 
-    ejb_remote_type_var = tk.StringVar(value="EJB 3.0")
-    ejb3_rb = ctk.CTkRadioButton(ejb_remote_type_frame, text="EJB 3.0", variable=ejb_remote_type_var, value="EJB 3.0")
-    ejb3_rb.grid(row=0, column=0, padx=5)
-    ejb2_rb = ctk.CTkRadioButton(ejb_remote_type_frame, text="EJB 2.0", variable=ejb_remote_type_var, value="EJB 2.0")
-    ejb2_rb.grid(row=0, column=1, padx=5)
+        ejb_remote_type_frame = CTkFrame(self, bg_color='#FFFFFF', fg_color="#FFFFFF")
+        ejb_remote_type_frame.grid(row=3, column=1, pady=5, padx=(10, 5), sticky="w")
 
-    # EJB Remote Interface
-    ejb_interface_label = ctk.CTkLabel(root2, text="Interface del EJB Remoto:")
-    ejb_interface_label.grid(row=4, column=0, sticky="w", pady=5)
-    ejb_interface_entry = ctk.CTkEntry(root2, width=250)
-    ejb_interface_entry.grid(row=4, column=1, pady=5, padx=10, sticky="w")
-    ejb_interface_button = ctk.CTkButton(root2, text="Buscar Interface")
-    ejb_interface_button.grid(row=4, column=1, sticky="e", padx=10)
+        ejb_remote_type_var = tk.StringVar(value="EJB 3.0")
+        ejb3_rb = CTkRadioButton(ejb_remote_type_frame, text="EJB 3.0", variable=ejb_remote_type_var, value="EJB 3.0", text_color="black", fg_color='#84bfc4', radiobutton_height=18, radiobutton_width=18, bg_color='#FFFFFF')
+        ejb3_rb.grid(row=0, column=0, padx=5)
+        ejb2_rb = CTkRadioButton(ejb_remote_type_frame, text="EJB 2.0", variable=ejb_remote_type_var, value="EJB 2.0", text_color="black", fg_color='#84bfc4', radiobutton_height=18, radiobutton_width=18, bg_color='#FFFFFF')
+        ejb2_rb.grid(row=0, column=1, padx=5)
 
-    # Deployment Server Parameters
-    deployment_server_label = ctk.CTkLabel(root2, text="Parámetros Servidor Despliegue")
-    deployment_server_label.grid(row=5, column=0, columnspan=2, pady=10)
+        ejb_interface_label = CTkLabel(self, text="Interface del EJB Remoto:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        ejb_interface_label.grid(row=4, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
+        ejb_interface_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        ejb_interface_entry.grid(row=4, column=1, padx=(10, 5), pady=(10, 2), sticky="ew")
+        ejb_interface_button = CTkButton(self, text="Buscar Interface", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        ejb_interface_button.grid(row=4, column=2, sticky="e", padx=(5, 10))
 
-    ip_label = ctk.CTkLabel(root2, text="IP Servidor:")
-    ip_label.grid(row=6, column=0, sticky="w", pady=5)
-    ip_entry = ctk.CTkEntry(root2, width=250)
-    ip_entry.grid(row=6, column=1, pady=5, padx=10, sticky="w")
+        deployment_server_label = CTkLabel(self, text="Parámetros Servidor Despliegue", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        deployment_server_label.grid(row=5, column=0, columnspan=3, pady=5)
 
-    port_label = ctk.CTkLabel(root2, text="Puerto:")
-    port_label.grid(row=7, column=0, sticky="w", pady=5)
-    port_entry = ctk.CTkEntry(root2, width=250)
-    port_entry.grid(row=7, column=1, pady=5, padx=10, sticky="w")
+        deployment_server_frame = CTkFrame(self, bg_color='#FFFFFF', fg_color="#FFFFFF")
+        deployment_server_frame.grid(row=6, column=0, columnspan=3, pady=5, padx=(10, 5), sticky="ew")
 
-    # Remote EJB Parameters
-    remote_ejb_label = ctk.CTkLabel(root2, text="Parámetros EJB remoto")
-    remote_ejb_label.grid(row=8, column=0, columnspan=2, pady=10)
+        ip_label = CTkLabel(deployment_server_frame, text="IP Servidor:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        ip_label.grid(row=0, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
+        ip_entry = CTkEntry(deployment_server_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        ip_entry.grid(row=0, column=1, padx=(10, 5), pady=(10, 2), sticky="ew")
 
-    remote_ejb_name_label = ctk.CTkLabel(root2, text="Nombre Servidor EJB:")
-    remote_ejb_name_label.grid(row=9, column=0, sticky="w", pady=5)
-    remote_ejb_name_entry = ctk.CTkEntry(root2, width=250)
-    remote_ejb_name_entry.grid(row=9, column=1, pady=5, padx=10, sticky="w")
+        port_label = CTkLabel(deployment_server_frame, text="Puerto:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        port_label.grid(row=0, column=2, sticky="w", padx=(10, 5), pady=(10, 2))
+        port_entry = CTkEntry(deployment_server_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        port_entry.grid(row=0, column=3, padx=(10, 5), pady=(10, 2), sticky="ew")
 
-    remote_ip_label = ctk.CTkLabel(root2, text="Dirección IP:")
-    remote_ip_label.grid(row=10, column=0, sticky="w", pady=5)
-    remote_ip_entry = ctk.CTkEntry(root2, width=250)
-    remote_ip_entry.grid(row=10, column=1, pady=5, padx=10, sticky="w")
+        remote_ejb_label = CTkLabel(self, text="Parámetros EJB remoto", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        remote_ejb_label.grid(row=7, column=0, columnspan=3, pady=5)
 
-    remote_port_label = ctk.CTkLabel(root2, text="Puerto:")
-    remote_port_label.grid(row=11, column=0, sticky="w", pady=5)
-    remote_port_entry = ctk.CTkEntry(root2, width=250)
-    remote_port_entry.grid(row=11, column=1, pady=5, padx=10, sticky="w")
+        remote_ejb_frame = CTkFrame(self, bg_color='#FFFFFF', fg_color="#FFFFFF")
+        remote_ejb_frame.grid(row=8, column=0, columnspan=3, pady=5, padx=(10, 5), sticky="ew")
 
-    user_label = ctk.CTkLabel(root2, text="Usuario:")
-    user_label.grid(row=12, column=0, sticky="w", pady=5)
-    user_entry = ctk.CTkEntry(root2, width=250)
-    user_entry.grid(row=12, column=1, pady=5, padx=10, sticky="w")
+        remote_ejb_name_label = CTkLabel(remote_ejb_frame, text="Nombre Servidor EJB:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        remote_ejb_name_label.grid(row=0, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
+        remote_ejb_name_entry = CTkEntry(remote_ejb_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        remote_ejb_name_entry.grid(row=0, column=1, padx=(10, 5), pady=(10, 2), sticky="ew")
 
-    password_label = ctk.CTkLabel(root2, text="Password:")
-    password_label.grid(row=13, column=0, sticky="w", pady=5)
-    password_entry = ctk.CTkEntry(root2, width=250, show="*")
-    password_entry.grid(row=13, column=1, pady=5, padx=10, sticky="w")
+        remote_ip_label = CTkLabel(remote_ejb_frame, text="Dirección IP:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        remote_ip_label.grid(row=1, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
+        remote_ip_entry = CTkEntry(remote_ejb_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        remote_ip_entry.grid(row=1, column=1, padx=(10, 5), pady=(10, 2), sticky="ew")
 
-    jndi_label = ctk.CTkLabel(root2, text="Nombre JNDI:")
-    jndi_label.grid(row=14, column=0, sticky="w", pady=5)
-    jndi_entry = ctk.CTkEntry(root2, width=250)
-    jndi_entry.grid(row=14, column=1, pady=5, padx=10, sticky="w")
+        remote_port_label = CTkLabel(remote_ejb_frame, text="Puerto:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        remote_port_label.grid(row=1, column=2, sticky="w", padx=(10, 5), pady=(10, 2))
+        remote_port_entry = CTkEntry(remote_ejb_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        remote_port_entry.grid(row=1, column=3, padx=(10, 5), pady=(10, 2), sticky="ew")
 
-    # Buttons
-    buttons_frame = ctk.CTkFrame(root2)
-    buttons_frame.grid(row=15, column=0, columnspan=2, pady=10)
-    
-    back_button = ctk.CTkButton(buttons_frame, text="Back")
-    back_button.grid(row=0, column=0, padx=5)
-    next_button = ctk.CTkButton(buttons_frame, text="Next", state="disabled")
-    next_button.grid(row=0, column=1, padx=5)
-    finish_button = ctk.CTkButton(buttons_frame, text="Finish")
-    finish_button.grid(row=0, column=2, padx=5)
-    cancel_button = ctk.CTkButton(buttons_frame, text="Cancel")
-    cancel_button.grid(row=0, column=3, padx=5)
+        user_label = CTkLabel(remote_ejb_frame, text="Usuario:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        user_label.grid(row=2, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
+        user_entry = CTkEntry(remote_ejb_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        user_entry.grid(row=2, column=1, padx=(10, 5), pady=(10, 2), sticky="ew")
 
-create_widgets_2()
-root2.mainloop()
+        password_label = CTkLabel(remote_ejb_frame, text="Password:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        password_label.grid(row=2, column=2, sticky="w", padx=(10, 5), pady=(10, 2))
+        password_entry = CTkEntry(remote_ejb_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, show="*", text_color="black")
+        password_entry.grid(row=2, column=3, padx=(10, 5), pady=(10, 2), sticky="ew")
+
+        jndi_label = CTkLabel(remote_ejb_frame, text="Nombre JNDI:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        jndi_label.grid(row=3, column=0, sticky="w", padx=(10, 5), pady=(10, 2))
+        jndi_entry = CTkEntry(remote_ejb_frame, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=2.5, border_width=3, text_color="black")
+        jndi_entry.grid(row=3, column=1, padx=(10, 5), pady=(10, 2), sticky="ew")
+
+        buttons_frame = CTkFrame(self, bg_color='#FFFFFF', fg_color="#FFFFFF")
+        buttons_frame.grid(row=15, column=0, columnspan=3, pady=10)
+
+        back_button = CTkButton(buttons_frame, text="Back", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        back_button.grid(row=0, column=0, padx=5)
+        next_button = CTkButton(buttons_frame, text="Next", state="disabled", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        next_button.grid(row=0, column=1, padx=5)
+        finish_button = CTkButton(buttons_frame, text="Finish", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        finish_button.grid(row=0, column=2, padx=5)
+        cancel_button = CTkButton(buttons_frame, text="Cancel", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        cancel_button.grid(row=0, column=3, padx=5)
+
+if __name__ == '__main__':
+    app = VentanaPaso6()
+    app.mainloop()
--- VentanaPaso7.py
+++ VentanaPaso7.py
@@ -1,61 +1,63 @@
 import customtkinter as ctk
+from customtkinter import *
 import tkinter as tk
 
+class VentanaPaso7(CTk):
+    def __init__(self):
+        super().__init__()
+        # Configurar la ventana principal
+        self.title("Generar EJB")
+        self.geometry("900x700")
+        self.resizable(width=False, height=False)
+        self.config(bg="#FFFFFF")
 
-# Create the main window
-root3 = ctk.CTk()
-root3.title("Generar EJB")
-root3.geometry("500x400")
+        self.columnconfigure(1, weight=1)
 
-def create_widgets_3():
-    # Title label
-    title_label = ctk.CTkLabel(root3, text="Generar EJB", font=ctk.CTkFont(size=16, weight="bold"))
-    title_label.grid(row=0, column=0, columnspan=2, pady=10)
+        configuration_frame = CTkFrame(self, bg_color="#FFFFFF", fg_color="#FFFFFF")
+        configuration_frame.grid(row=0, column=0, columnspan=3, sticky="ew")
 
-    # Subtitle label
-    subtitle_label = ctk.CTkLabel(root3, text="Este Wizard el EJB de un servicio existente")
-    subtitle_label.grid(row=1, column=0, columnspan=2)
+        configuration_label = CTkLabel(configuration_frame, text="Generar EJB", font=("Arial", 14, "bold"), bg_color="#FFFFFF", fg_color="#FFFFFF")
+        configuration_label.grid(row=0, column=0, columnspan=3, pady=(10, 5), padx=10, sticky="w")
 
-    # EJB project container
-    ejb_container_label = ctk.CTkLabel(root3, text="Proyecto EJB contenedor:")
-    ejb_container_label.grid(row=2, column=0, sticky="w", pady=5)
-    ejb_container_entry = ctk.CTkEntry(root3, width=250)
-    ejb_container_entry.grid(row=2, column=1, pady=5, padx=10, sticky="w")
-    ejb_container_button = ctk.CTkButton(root3, text="Buscar Proyecto")
-    ejb_container_button.grid(row=2, column=1, sticky="e", padx=10)
+        description_label = CTkLabel(configuration_frame, text="Este Wizard el EJB de un servicio existente", bg_color="#FFFFFF", fg_color="#FFFFFF")
+        description_label.grid(row=1, column=0, columnspan=3, pady=(5, 5), padx=10, sticky="w")
 
-    # Service
-    service_label = ctk.CTkLabel(root3, text="Servicio:")
-    service_label.grid(row=3, column=0, sticky="w", pady=5)
-    service_entry = ctk.CTkEntry(root3, width=250)
-    service_entry.grid(row=3, column=1, pady=5, padx=10, sticky="w")
-    service_button = ctk.CTkButton(root3, text="Buscar Servicio")
-    service_button.grid(row=3, column=1, sticky="e", padx=10)
+        ejb_container_label = CTkLabel(self, text="Proyecto EJB contenedor:", bg_color='#FFFFFF', fg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        ejb_container_label.grid(row=2, column=0, sticky="w", pady=5, padx=10)
+        ejb_container_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=25, border_width=3, text_color="black")
+        ejb_container_entry.grid(row=2, column=1, pady=5, padx=10, sticky="w")
+        ejb_container_button = CTkButton(self, text="Buscar Proyecto", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        ejb_container_button.grid(row=2, column=2, sticky="e", padx=10)
 
-    # JNDI Name
-    jndi_label = ctk.CTkLabel(root3, text="Nombre JNDI:")
-    jndi_label.grid(row=4, column=0, sticky="w", pady=5)
-    jndi_entry = ctk.CTkEntry(root3, width=250)
-    jndi_entry.grid(row=4, column=1, pady=5, padx=10, sticky="w")
+        service_label = CTkLabel(self, text="Servicio:", bg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        service_label.grid(row=3, column=0, sticky="w", pady=5, padx=10)
+        service_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=25, border_width=3, text_color="black")
+        service_entry.grid(row=3, column=1, pady=5, padx=10, sticky="w")
+        service_button = CTkButton(self, text="Buscar Servicio", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        service_button.grid(row=3, column=2, sticky="e", padx=10)
 
-    # EJB Name
-    ejb_name_label = ctk.CTkLabel(root3, text="Nombre del EJB:")
-    ejb_name_label.grid(row=5, column=0, sticky="w", pady=5)
-    ejb_name_entry = ctk.CTkEntry(root3, width=250)
-    ejb_name_entry.grid(row=5, column=1, pady=5, padx=10, sticky="w")
+        jndi_label = CTkLabel(self, text="Nombre JNDI:", bg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        jndi_label.grid(row=4, column=0, sticky="w", pady=5, padx=10)
+        jndi_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=25, border_width=3, text_color="black")
+        jndi_entry.grid(row=4, column=1, pady=5, padx=10, sticky="w")
 
-    # Buttons
-    buttons_frame = ctk.CTkFrame(root3)
-    buttons_frame.grid(row=6, column=0, columnspan=2, pady=10)
-    
-    back_button = ctk.CTkButton(buttons_frame, text="Back")
-    back_button.grid(row=0, column=0, padx=5)
-    next_button = ctk.CTkButton(buttons_frame, text="Next", state="disabled")
-    next_button.grid(row=0, column=1, padx=5)
-    finish_button = ctk.CTkButton(buttons_frame, text="Finish")
-    finish_button.grid(row=0, column=2, padx=5)
-    cancel_button = ctk.CTkButton(buttons_frame, text="Cancel")
-    cancel_button.grid(row=0, column=3, padx=5)
+        ejb_name_label = CTkLabel(self, text="Nombre del EJB:", bg_color="#FFFFFF", text_color="black", font=("Arial", 12, "bold"))
+        ejb_name_label.grid(row=5, column=0, sticky="w", pady=5, padx=10)
+        ejb_name_entry = CTkEntry(self, bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', height=25, border_width=3, text_color="black")
+        ejb_name_entry.grid(row=5, column=1, pady=5, padx=10, sticky="w")
 
-create_widgets_3()
-root3.mainloop()
+        buttons_frame = CTkFrame(self, bg_color="#FFFFFF", fg_color="#FFFFFF")
+        buttons_frame.grid(row=6, column=0, columnspan=3, pady=10)
+
+        back_button = CTkButton(buttons_frame, text="Back", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        back_button.grid(row=0, column=0, padx=5)
+        next_button = CTkButton(buttons_frame, text="Next", state="disabled", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        next_button.grid(row=0, column=1, padx=5)
+        finish_button = CTkButton(buttons_frame, text="Finish", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        finish_button.grid(row=0, column=2, padx=5)
+        cancel_button = CTkButton(buttons_frame, text="Cancel", bg_color='#FFFFFF', fg_color='#84bfc4', border_color='#84bfc4', hover_color='#41848a', text_color="black", font=("Arial", 12, "bold"))
+        cancel_button.grid(row=0, column=3, padx=5)
+
+if __name__ == '__main__':
+    app = VentanaPaso7()
+    app.mainloop()
