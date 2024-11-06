1be48ba32382e87c28dd1924a1868ddb9397646b
Hugo Martinez Garcia <hmartinez@teknei.com>
2024-11-06 14:49:14 +0100

    test paso 1 funcionando

--- test_initPaso1.py
+++ test_initPaso1.py
@@ -4,61 +4,64 @@ import VentanaPaso1 as paso1
 import os
 
 class TestSaveToYAML(unittest.TestCase):
-    @patch('VentanaPaso1.Worker')  
+    @patch('VentanaPaso1.Worker', wraps=paso1.Worker)  
     @patch('plugin.utils.writeConfig')
     @patch("VentanaPaso1.logging")
     @patch.object(paso1.Paso1, 'close_loading_frame', new=Mock())
-    def test_save_to_yaml(self, mock_logging, mock_write_config, mock_worker):
+    @patch.object(paso1.Paso1, 'update_progress', return_value=None)  # Mockear update_progress
+    def test_save_to_yaml(self,mock_update_progress, mock_logging, mock_write_config, mock_worker):
         # Mock de Worker y su método run_copy
-        mock_worker_instance = mock_worker.return_value.__enter__.return_value
+        mock_worker_instance = mock_worker.return_value
         mock_worker_instance.run_copy = MagicMock()
 
         # Crear una instancia de la clase a probar y configurar las variables necesarias
         instancia = paso1.Paso1(self)
-        instancia.language_options = ["Inglés", "Francés", "Castellano", "Euskera"]
-        instancia.language_vars = [MagicMock(get=lambda: False), MagicMock(get=lambda: True),
-                                   MagicMock(get=lambda: True), MagicMock(get=lambda: False)]
+        instancia.language_options = ["Castellano", "Euskera", "Inglés", "Francés"]
+        instancia.language_vars = [MagicMock(get=lambda: True), MagicMock(get=lambda: True),
+                                   MagicMock(get=lambda: False), MagicMock(get=lambda: False)]
         instancia.default_language_var = MagicMock(get=lambda: "Castellano")
         instancia.entry_code = MagicMock(get=lambda: "proyecto_test")
         instancia.security_var = MagicMock(get=lambda: "Si")
         instancia.entry_war = MagicMock(get=lambda: "war_test")
         instancia.security_yes_radio = MagicMock(get=lambda: True)
         instancia.default_language_combobox = MagicMock(get=lambda: "Castellano")
-        instancia.entry_location = MagicMock(get=lambda: "C:\\Users\\mllorente\\Desktop\\Entornos_UDA\\workspaces\\workspace_2020_v5\\udaPlugin\\plugin\\test_paso1")
-
+        instancia.entry_location = MagicMock(get=lambda: "C:/aplic/copier/test")
+        
         # Ejecutar el método save_to_yaml
         instancia.save_to_yaml()
-        
+
+        mock_update_progress.assert_called()
+
         # Datos esperados para la verificación
         esperado_yaml_data = {
-            "i18n_app": ["Francés", "Castellano"],
+            "i18n_app": ["Castellano", "Euskera"],
             "i18n_default_app": "Castellano",
             "project_name": "proyecto_test",
             "security_app": "Si",
             "war_project_name": "war_test",
             "xlnets": True,
             "defaultLanguage": "es",
-            "availableLangs": "es, eu ,fr"
+            "availableLangs": "es, eu"
         }
        
         # Verificar que Worker fue llamado con los parámetros correctos
         mock_worker.assert_called_once_with(
-            src_path="c:\\Users\\mllorente\\Desktop\\Entornos_UDA\\workspaces\\workspace_2020_v5\\udaPlugin\\plugin\\templates\\proyecto",
+            src_path="c:\\aplic\\copier\\udaPlugin\\templates\\proyecto",
             overwrite=True,
-            dst_path="C:\\Users\\mllorente\\Desktop\\Entornos_UDA\\workspaces\\workspace_2020_v5\\udaPlugin\\plugin\\test_paso1",
+            dst_path="C:/aplic/copier/test",
             data=esperado_yaml_data,
-            exclude=["*i18n_en*", "*EJB"]
+            exclude=['*i18n_en*', '*i18n_fr*', '*EJB']
         )
 
-        # Verificar que run_copy fue llamado dentro de Worker
-        mock_worker_instance.run_copy.assert_called_once()
+        # Verificar que run_copy no fue llamado por el mock
+        self.assertEqual(mock_worker_instance.run_copy.called, False)
 
         # Verificar que writeConfig fue llamado con las rutas especificadas
         mock_write_config.assert_called_once_with(
             "RUTA", {
-                "ruta_classes": "C:\\Users\\mllorente\\Desktop\\Entornos_UDA\\workspaces\\workspace_2020_v5\\udaPlugin\\plugin\\test_paso1",
-                "ruta_war": "C:\\Users\\mllorente\\Desktop\\Entornos_UDA\\workspaces\\workspace_2020_v5\\udaPlugin\\plugin\\test_paso1",
-                "ruta_ultimo_proyecto": "C:\\Users\\mllorente\\Desktop\\Entornos_UDA\\workspaces\\workspace_2020_v5\\udaPlugin\\plugin\\test_paso1"
+                "ruta_classes": "C:/aplic/copier/test",
+                "ruta_war": "C:/aplic/copier/test",
+                "ruta_ultimo_proyecto": "C:/aplic/copier/test"
             }
         )
 
