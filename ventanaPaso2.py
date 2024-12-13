ca0d4dd8b9145e861c8ddeda98ad72e3ed078134
hmartinez85 <34453480+hmartinez85@users.noreply.github.com>
2024-12-13 08:57:30 +0100

    Mejora por si vienen 0 tablas

--- ventanaPaso2.py
+++ ventanaPaso2.py
@@ -222,7 +222,7 @@ class PaginaDos(CTkFrame):
     def populate_scrollable_frame(self, frame, tables_original):
         self.var_list = []
         total_pasos = len(tables_original) + 1
-        pasos_por_parte = total_pasos // 8
+        pasos_por_parte = max(total_pasos // 8, 1)
 
         # Campo de entrada para autocompletado en la parte superior
         
